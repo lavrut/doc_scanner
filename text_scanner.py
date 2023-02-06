79fd437dd041762924e8946d700fcd2a9873d495
ChewyLemonade <32437530+lavrut@users.noreply.github.com>
2023-02-06 02:51:15 -0500

    Update text_scanner.py

--- text_scanner.py
+++ text_scanner.py
@@ -1,20 +1,14 @@
-name = input('Enter file: ')
-document = open(name, 'r')
-counts = dict()
+name = input("Enter file name: ")
 
-for line in document:
-    words = line.split()
-    for word in words:
-      counts[word] = counts.get(word, 0) + 1
-            
-word_freq= None
-common_word = None
-for word, count in list(counts.items()):
-    if word_freq is None or count > word_freq:
-        common_word = word
-        word_freq = count
-        
-print("The most common word in the document is:", common_word + ".")
-print("The word occurs", word_freq, "times.")
+with open(name, 'r') as document:
+    word_counts = {}
+    for line in document:
+        words = line.split()
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1
 
+most_common_word = max(word_counts, key=word_counts.get)
+most_common_word_count = word_counts[most_common_word]
 
+print(f"The most common word in the document is '{most_common_word}'.")
+print(f"It appears {most_common_word_count} times.")
